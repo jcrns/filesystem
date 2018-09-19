2dbb9ea414394840dfb8d5e8b51c91b87f9c37ac
Jayden-Cummings <jayden.cummings@hgs.hiddengeniusproject.org>
2018-09-18 18:09:43 -0700

    first commit

---
+++ file.sh
@@ -0,0 +1,34 @@
+pwd 						# Shows the directory your in
+cd ~/Desktop 				# Goes home then into the desktop
+mkdir website_filesystem 	# Making a folder named website_filesystem
+cd website_filesystem  		# Going in website_filesystem
+touch index.html 			# Creating index.html file
+mkdir html css js img extra # Creating website folders
+cd html 					# Going into html folder
+touch pg1.html				# Creating 1st html page
+touch pg2.html				# Creating 2nd html page
+cd ../css					# Going back a directory then going into css folder
+touch pg1.css				# Creating 1st css file
+touch pg2.css				# Creating 2nd css file
+touch index.css				# Creating main css file
+cd ../js					# Going back a directory then going into javascript folder
+touch pg1.js				# Creating 1st javascript file
+touch pg2.js				# Creating 2nd javascript file
+touch index.js				# Creating main javascript file
+cd ../extra					# Going back a directory the going into extra folder
+mkdir audio 				# Making audio directory
+mkdir video 				# Making video directory
+
+#cd - change directory
+#pwd - Print working directory
+#ls - List content in directory
+#mkdir - creates new folder
+#touch - creates new file
+#curl - o - Downloads image from internet
+#sh - Runs shell script file
+#mv - Renames and mvoes files and folders
+#open - opens file
+#cd ../ - Goes to previous directory then goes to any directory after /
+#cd ~ - Home
+#cd / Root
+#cd ~/Desktop - goes to root folder than desktop
